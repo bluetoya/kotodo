6e762e09731f001d7c1bd94cde73365f4ef5a92e
toyablue <hjahn934@gmail.com>
2024-05-06 23:34:58 +0900

    feat: create repo

---
+++ src/test/kotlin/com/bluetoya/kotodo/KotodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bluetoya.kotodo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
