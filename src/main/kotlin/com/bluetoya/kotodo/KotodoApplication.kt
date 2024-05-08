42ee00c4dd647acd70d716a738346d4c4b05f97f
toyablue <hjahn934@gmail.com>
2024-05-08 22:36:58 +0900

    feat: controller test

--- src/main/kotlin/com/bluetoya/kotodo/KotodoApplication.kt
+++ src/main/kotlin/com/bluetoya/kotodo/KotodoApplication.kt
@@ -1,5 +1,6 @@
 package com.bluetoya.kotodo
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class KotodoApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotodoApplication>(*args)
+	runApplication<KotodoApplication>(*args) {
+		setBannerMode(Banner.Mode.OFF)
+	}
 }
