dd43f3dd0f01bac55c138e6b5da66b15aa99d673
bluetoya <hjahn934@gmail.com>
2024-06-23 00:29:12 +0900

    fix: auto generate를 위해 id에 기본 값 설정

--- src/main/kotlin/com/bluetoya/kotodo/domain/Task.kt
+++ src/main/kotlin/com/bluetoya/kotodo/domain/Task.kt
@@ -9,7 +9,7 @@ import java.time.LocalDateTime
 @Entity
 class Task(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+    val id: Long = 0,
     var name: String,
     var taskGroup: String? = null,
     var description: String? = null,
