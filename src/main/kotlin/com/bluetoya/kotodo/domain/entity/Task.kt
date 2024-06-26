7edb017c867423f0e772b8b67046bfda8dc78b50
bluetoya <hjahn934@gmail.com>
2024-06-27 01:21:05 +0900

    feat: entity 폴더 생성 및 엔티티 클래스 이동

--- src/main/kotlin/com/bluetoya/kotodo/domain/entity/Task.kt
+++ src/main/kotlin/com/bluetoya/kotodo/domain/entity/Task.kt
@@ -1,13 +1,21 @@
-package com.bluetoya.kotodo.domain
+package com.bluetoya.kotodo.domain.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import java.time.LocalDateTime
 
 @Entity
-class TaskGroup(
+class Task(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    var name: String
+    var name: String,
+    var taskGroup: String? = null,
+    var description: String? = null,
+    var isDone: Boolean,
+    var dueDate: LocalDateTime
 )
+
+
+
--- src/main/kotlin/com/bluetoya/kotodo/domain/entity/TaskGroup.kt
+++ src/main/kotlin/com/bluetoya/kotodo/domain/entity/TaskGroup.kt
@@ -1,21 +1,13 @@
-package com.bluetoya.kotodo.domain
+package com.bluetoya.kotodo.domain.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import java.time.LocalDateTime
 
 @Entity
-class Task(
+class TaskGroup(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    var name: String,
-    var taskGroup: String? = null,
-    var description: String? = null,
-    var isDone: Boolean,
-    var dueDate: LocalDateTime
+    var name: String
 )
-
-
-
