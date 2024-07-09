ceeaf77d5aa5f5ac5241eddf33b0e32e110cd067
bluetoya <hjahn934@gmail.com>
2024-07-09 23:58:09 +0900

    feat: create TaskItemController

---
+++ src/main/kotlin/com/bluetoya/kotodo/controller/TaskItemController.kt
@@ -0,0 +1,9 @@
+package com.bluetoya.kotodo.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/task/item")
+class TaskItemController {
+}
